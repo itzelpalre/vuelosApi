5cceb4c468e18a3e08479f2e830bdd4006da3480
itzelpalre <itzelpalmareyes@gmail.com>
2023-09-07 11:51:05 -0600

    Commit 1

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "fazt";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "faztpassword";
-export const DB_DATABASE = process.env.DB_DATABASE || "companydb";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "RedAviation@2023_";
+export const DB_DATABASE = process.env.DB_DATABASE || "apidemo";
 export const DB_PORT = process.env.DB_PORT || 3306;
